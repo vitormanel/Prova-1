b30bd113af26a61cfedabf150a562c466616f298
Vitor Manuel <Vitormanuel.567b@outlook.com>
2022-02-14 15:30:30 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int a, b, c;
+
+                Console.WriteLine("Digite valor 1: ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite valor 1: ");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite valor 1: ");
+                c = Convert.ToInt32(Console.ReadLine());
+
+                if (a + b > c && a + c > b && b + c > a)
+                {
+                    Console.WriteLine("Os 3 lados formam um triangulo!\n");
+                    if (a == b && a == c)
+                        Console.WriteLine("Equilatero\n");
+                        
+                    else
+                        if (a == b || a == c || b == c)
+                        Console.WriteLine("Isosceles\n");
+                    else
+                        Console.WriteLine("Escaleno\n");
+                
+            }
+                else
+                    Console.WriteLine("Os 3 lados NAO formam um trinagulo!\n");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Prova1.ConsoleApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Prova1.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Prova 1 - Academia do programador - Vitor Manuel R. \n");
+
+            String opcao;
+            do
+            {
+                int a, b, c;
+
+                Console.WriteLine("Digite o Primeiro valor: ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite o Segundo valor: ");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite o Terceiro valor: ");
+                c = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Digite 1 para verificar o tipo do triangulo.");
+                Console.WriteLine("Digite S para sair");
+                opcao = Console.ReadLine();
+
+                if (opcao.Equals("s", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    break;
+                }
+
+                if (opcao == "1")
+                {
+                    Console.WriteLine(" Os 3 lados formam um triangulo! \n");
+
+                    //Eqüilátero: Todos os lados iguais (Funcionando!)
+                    if (a == b && a == c)
+                    {
+                        Console.WriteLine(" Os 3 lados formam um Equilatero! ");
+                        Console.ReadLine();
+                    }
+
+                    //Isósceles: Dois lados iguais (Funcionando)
+                    else if (a == b || a == c || b == c)
+                    {
+                        Console.WriteLine(" Os 3 lados formam um Isoceles! ");
+                        Console.ReadLine();
+                    }
+
+                    //Escaleno: Todos os lados diferentes (Funcionando!)
+                    else if (a != b && a != c && c != b)
+                    {
+                        Console.WriteLine(" Os 3 lados formam um Escaleno! ");
+                        Console.ReadLine();
+                    }
+                    //Lados invalidos
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Triangulo invalido:\n");
+                        Console.WriteLine("Os 3 lados nao formam um triangulo!");
+                        Console.ReadLine();
+                        Console.ResetColor();
+                    }
+                    Console.ReadLine();
+                }
+            } while (opcao == "1" || opcao == "S");
+        }
+    }
+}
